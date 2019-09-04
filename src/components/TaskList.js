15633580a12c41592f356d222c3ba0c876e16811
Dariusz Okonski <darek200180@gmail.com>
2019-09-04 19:54:01 +0100

    add map method to display Tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Task from './Task';
 
-const TaskList = () => {
+const TaskList = (props) => {
+  const tasks = props.tasks.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
+
   return (
-    <div>
-      <h3>Lista Tasków</h3>
-      <Task />
-      <Task />
-      <Task />
-    </div>
+    <>
+      <div className="active">
+        <h3>Zadania do zrobienia ({tasks.length})</h3>
+        {tasks}
+      </div>
+
+      <hr />
+      <div className="done">
+        <h4>Zadania zrobione (0)</h4>
+      </div>
+      <hr />
+    </>
+
   );
 }
 
