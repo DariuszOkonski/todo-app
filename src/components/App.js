9c43cb910ec722c7707042e3f91aac7fd2b6a9ff
Dariusz Okonski <darek200180@gmail.com>
2019-09-04 18:52:30 +0100

    import component TaskList to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import AddTask from './AddTask';
+import TaskLIst from './TaskList';
 
 function App() {
   return (
     <div className="App">
       ToDo - app
-
       <AddTask />
+      <TaskLIst />
     </div>
   );
 }
