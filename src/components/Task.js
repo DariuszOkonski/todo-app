8fa09f8d605306c5ae2c691a323390ee33c4edde
Dariusz Okonski <darek200180@gmail.com>
2019-09-05 15:47:19 +0100

    amendmends in logic if current task is active or not

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,16 +2,36 @@ import React from 'react';
 
 const Task = (props) => {
   const { id, text, date, important, active, finishDate } = props.task;
+  const importantStyle = important ? { color: 'red' } : null;
 
-  return (
-    <div>
-      <p>
-        <strong>{text}</strong> - do <span>{date} </span>
-        {active && <button onClick={() => props.change(id)}>Done</button>}
-        <button onClick={() => props.delete(id)}>Remove</button>
-      </p>
-    </div>
-  );
+  if (active) {
+    return (
+      <div>
+        <p>
+          <strong style={importantStyle}>{text}</strong> - till <span>{date} </span>
+
+          <button onClick={() => props.change(id)}>Done</button>
+
+          <button onClick={() => props.delete(id)}>Remove</button>
+        </p>
+      </div>
+    );
+  } else {
+    const finish = new Date(finishDate).toLocaleString();
+
+    return (
+      <div>
+        <p>
+          <strong>{text}</strong> - <em>(deadline {date})</em><br />
+          - task finished at: <strong>{finish} </strong>
+
+
+          <button onClick={() => props.delete(id)}>Remove</button>
+        </p>
+      </div>
+    );
+  }
+
 }
 
 export default Task;
