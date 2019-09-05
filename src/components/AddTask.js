8567da9311f2dd624b5193c7f78e46d82f1f44ed
Dariusz Okonski <darek200180@gmail.com>
2019-09-05 18:22:52 +0100

    add implementation for methods handleText, handleChecked, handleDate, handleClick

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,23 +2,82 @@ import React, { Component } from 'react';
 import './AddTask.css';
 
 class AddTask extends Component {
+  minDate = new Date().toISOString().substr(0, 10);
+  maxDate = `${(this.minDate.substr(0, 4) * 1 + 1)}-12-31`;
+
   state = {
     text: '',
     checked: false,
-    date: '2019-09-05',
+    date: this.minDate,
+  }
+
+  handleText = (e) => {
+    this.setState({
+      text: e.target.value,
+    })
+  }
+
+  handleChecked = (e) => {
+    this.setState({
+      checked: e.target.checked,
+    })
+  }
+
+  handleDate = (e) => {
+    this.setState({
+      date: e.target.value,
+    })
+  }
+
+  handleClick = () => {
+    if (this.state.text.length < 3) {
+      alert('Task can not be shorter then 3 letters...');
+      return;
+    }
+
+    this.props.onAddTask(this.state);
+
+    this.setState({
+      text: '',
+      checked: false,
+      date: this.minDate,
+    })
   }
 
   render() {
+
     return (
       <div className="form">
-        <input type="text" placeholder="Add task..." value={this.state.text} />
-        <input type="checkbox" checked={this.state.checked} id="important" />
+        <input
+          type="text"
+          placeholder="Add task..."
+          value={this.state.text}
+          onChange={this.handleText}
+        />
+
+        <input
+          type="checkbox"
+          checked={this.state.checked}
+          id="important"
+          onChange={this.handleChecked}
+        />
         <label htmlFor="important">Priority</label>
         <br />
+
         <label htmlFor="date">Deadline:</label>
-        <input type="date" id="date" value={this.state.date} min='2019-09-05' max='2020-12-31' />
+        <input
+          type="date"
+          id="date"
+          value={this.state.date}
+          min={this.minDate}
+          max={this.maxDate}
+          onChange={this.handleDate}
+        />
         <br />
-        <button>Add New Task</button>
+
+        <button
+          onClick={this.handleClick}
+        >Add New Task</button>
         <hr />
       </div>
     );
