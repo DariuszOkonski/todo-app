286a0101f7697120bd0cb3d30ddd5bccc586dc43
Dariusz Okonski <darek200180@gmail.com>
2019-09-05 16:03:32 +0100

    create component AddTask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import './AddTask.css';
 
 class AddTask extends Component {
-  state = {}
+  state = {
+    text: '',
+    checked: false,
+    date: '2019-09-05',
+  }
 
   render() {
     return (
       <div className="form">
-        <div>
-          Add Task
-        </div>
+        <input type="text" placeholder="Add task..." value={this.state.text} />
+        <input type="checkbox" checked={this.state.checked} id="important" />
+        <label htmlFor="important">Priority</label>
+        <br />
+        <label htmlFor="date">Deadline:</label>
+        <input type="date" id="date" value={this.state.date} min='2019-09-05' max='2020-12-31' />
+        <br />
+        <button>Add New Task</button>
         <hr />
       </div>
     );
