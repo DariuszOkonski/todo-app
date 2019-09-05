3abe585a94c56fff73747a055205e5c5c60fb8a2
Dariusz Okonski <darek200180@gmail.com>
2019-09-05 15:27:01 +0100

    implement logic if task has status to do or done and displaying in proper row

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,18 +2,24 @@ import React from 'react';
 import Task from './Task';
 
 const TaskList = (props) => {
-  const tasks = props.tasks.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
+  const active = props.tasks.filter(task => task.active);
+  const done = props.tasks.filter(task => !task.active);
+
+  const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />);
+  const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
 
   return (
     <>
       <div className="active">
-        <h3>Zadania do zrobienia ({tasks.length})</h3>
-        {tasks}
+        <h3>Tasks to do ({activeTasks.length})</h3>
+        {activeTasks.length > 0 ? activeTasks : <p>Nothing to do</p>}
       </div>
 
       <hr />
       <div className="done">
-        <h4>Zadania zrobione (0)</h4>
+        <h4>Done Tasks ({doneTasks.length})</h4>
+        {doneTasks.length > 5 && <strong style={{ color: 'red' }}>Displaying only last 5 tasks</strong>}
+        {doneTasks.slice(0, 5)}
       </div>
       <hr />
     </>
