23590f87c560a899ff52471929ade85b5f27a072
Dariusz Okonski <darek200180@gmail.com>
2019-09-05 15:23:46 +0100

    change language from Polish to English

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ import TaskLIst from './TaskList';
 class App extends Component {
   state = {
     tasks: [
-      { id: 0, text: 'zagrać wreszcie w Wiedźmina 3', date: '2018-02-15', important: true, active: true, finishDate: null },
-      { id: 1, text: 'zrobić dobry uczynek', date: '2020-11-12', important: false, active: true, finishDate: null },
+      { id: 0, text: 'zagrać wreszcie w Wiedźmina 3', date: '2018-02-15', important: true, active: false, finishDate: null },
+      { id: 1, text: 'zrobić dobry uczynek', date: '2020-11-12', important: false, active: false, finishDate: null },
       { id: 2, text: 'pomalować dom po sylwestrze', date: '2019-09-11', important: false, active: true, finishDate: null },
       { id: 3, text: 'schudnąć 30 kilogramów', date: '2019-05-20', important: true, active: true, finishDate: null },
       { id: 4, text: 'sprzedać butelki po piwie (20 skrzynek)', date: '2020-11-12', important: false, active: true, finishDate: null },
